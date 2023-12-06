afee4ef7bd6d064233f577513e7c066d3f04d4d9
jilherme <rosa.guilhermediogo@gmail.com>
2023-12-05 22:58:44 -0300

    test: cypress custom commands

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -23,9 +23,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
 
